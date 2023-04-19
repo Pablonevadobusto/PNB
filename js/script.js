d439771e2566d49aa53e04ed0207336810b39a82
Pablo Nevado-Busto <64086474+Pablonevadobusto@users.noreply.github.com>
2023-04-19 23:15:16 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+/*=========== MIXITUP FILTER PORTFOLIO ==============*/
+
+let mixerPortfolio = mixitup('.card-content', {
+    selectors: {
+        target: '.card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Link active work */
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
